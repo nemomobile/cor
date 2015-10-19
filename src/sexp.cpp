96d075eb64cff2a787ff3af682fe2741bb479f21
David Greaves <david@dgreaves.com>
2015-10-19 17:28:32 +0000

    Migration to git.merproject.org
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/sexp.cpp
+++
@@ -1,9 +0,0 @@
-#include <cor/sexp_impl.hpp>
-
-namespace cor {
-namespace sexp {
-
-template
-void parse(std::basic_istream<char> &src, AbstractHandler &handler);
-
-}}
--- src/util.cpp
+++
@@ -1,9 +0,0 @@
-#include <cor/util.hpp>
-#include <cor/options.hpp>
-
-namespace cor
-{
-
-template class OptParse<std::string>;
-
-}
